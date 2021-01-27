e5fb6fdf9c7ff1031c1f40f0bceb8ddb609d1632
Ramona84 <ramona.rettig@outlook.de>
2021-01-28 00:44:40 +0100

    add app.py

---
+++ sample-application/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+AKUIS6VEP9M7KLD5UIO69
+
+@app.route("/")
+def hello():
+    return "Hello World! Hello World! Hello World! Hello World! Hello World! AKUIS6VEP9M7KLD5UIO69"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
